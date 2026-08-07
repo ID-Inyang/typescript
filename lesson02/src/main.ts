0e82c12c240a0e194b78fcffc872f1a91191f62e
BRID19 <74292639+BRID19@users.noreply.github.com>
2026-08-07 03:41:20 +0100

    Add guitar and band arrays with console logs to main.ts

--- lesson02/src/main.ts
+++ lesson02/src/main.ts
@@ -6,3 +6,16 @@ enum OperatingSystems {
 
 let myOS: OperatingSystems = OperatingSystems.windows;
 console.log(`My operating system is ${myOS}.`);
+
+let guitars = ["Fender", "Gibson", "Ibanez"];
+
+guitars.push("PRS");
+console.log(`My favorite guitars are: ${guitars.join(", ")}.`);
+guitars.unshift("Yamaha");
+console.log(`After adding Yamaha, my favorite guitars are: ${guitars.join(", ")}.`);
+
+let bands: string[] = ["Metallica", "Nirvana", "Queen"];
+bands.push("Pink Floyd");
+console.log(`My favorite bands are: ${bands.join(", ")}.`);
+bands.unshift("The Beatles");
+console.log(`After adding The Beatles, my favorite bands are: ${bands.join(", ")}.`);
